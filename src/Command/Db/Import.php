efc31719f8e1da7e181dd96412aeb07a34cbac02
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-08-10 17:01:04 +0100

    Add command `db:import` to import local database file into a WordPress instance

---
+++ src/Command/Db/Import.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WpEcs\Command\Db;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use WpEcs\WordpressInstance;
+
+class Import extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('db:import')
+            ->setDescription('Import a SQL file into a running WordPress instance')
+            ->addArgument('app', InputArgument::REQUIRED, 'Name of the application')
+            ->addArgument('env', InputArgument::REQUIRED, 'Environment to run in')
+            ->addArgument('filename', InputArgument::REQUIRED, 'The filename to import');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $instance = new WordpressInstance(
+            $input->getArgument('app'),
+            $input->getArgument('env')
+        );
+
+        $command = $instance->prepareCommand(
+            'wp --allow-root db import -',
+            [],
+            ['-i']
+        );
+        $process = new Process($command);
+
+        $fromFile = $input->getArgument('filename');
+        $fh = fopen($fromFile, 'r');
+        $process->setInput($fh);
+        $process->mustRun();
+        fclose($fh);
+
+        $output->writeln("<info>Success:</info> Database imported from <comment>$fromFile</comment>");
+    }
+}
