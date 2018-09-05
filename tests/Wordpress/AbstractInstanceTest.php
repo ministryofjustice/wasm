4a692fa214ca418638b297d546230f82f8f72eca
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-09-05 18:04:09 +0100

    Add tests for `AbstractInstance`
    - Add test for `execute` method
    - Add test for `importDatabase` method
    - Add test for `exportDatabase` method
    - Create private helper function to assist with creating new mockable instances of the abstract class `AbstractInstance`

--- tests/Wordpress/AbstractInstanceTest.php
+++ tests/Wordpress/AbstractInstanceTest.php
@@ -2,39 +2,14 @@
 
 use PHPUnit\Framework\TestCase;
 use WpEcs\Wordpress\AbstractInstance;
+use Symfony\Component\Process\Process;
+use org\bovigo\vfs\vfsStream;
 
 class AbstractInstanceTest extends TestCase
 {
-    /**
-     * @var AbstractInstance
-     */
-    protected $instance;
-
-    protected function setUp()
-    {
-        $instance = $this->getMockForAbstractClass(
-            AbstractInstance::class,
-            [],
-            '',
-            false,
-            true,
-            true,
-            ['env']
-        );
-        $this->instance = $instance;
-    }
-
     public function testEnv()
     {
-        $instance = $this->getMockForAbstractClass(
-            AbstractInstance::class,
-            [],
-            '',
-            false,
-            true,
-            true,
-            ['execute']
-        );
+        $instance = $this->newInstance(['execute']);
 
         $instance->expects($this->once())
                  ->method('execute')
@@ -47,5 +22,109 @@ class AbstractInstanceTest extends TestCase
             $actual = $instance->env('SERVER_NAME');
             $this->assertEquals('example.com', $actual);
         }
+    }
+
+    public function testExecute()
+    {
+        $command = 'command-to-execute';
+        $output  = 'Output from command';
+
+        $process = $this->createMock(Process::class);
+        $process->expects($this->once())
+                ->method('mustRun')
+                ->willReturnSelf();
+        $process->expects($this->atLeastOnce())
+                ->method('getOutput')
+                ->willReturn($output);
+
+        $instance = $this->newInstance(['newCommand']);
+        $instance->expects($this->atLeastOnce())
+                 ->method('newCommand')
+                 ->with($command)
+                 ->willReturn($process);
+
+        $actualOutput = $instance->execute($command);
+        $this->assertEquals($output, $actualOutput);
+    }
+
+    public function testImportDatabase()
+    {
+        $structure = ['database.sql' => 'SQL file content'];
+        $vfs = vfsStream::setup('root', null, $structure);
+        $fh = fopen("{$vfs->url()}/database.sql", 'r');
+
+        $process = $this->createMock(Process::class);
+        $process->expects($this->at(0))
+                ->method('setInput')
+                ->with($fh)
+                ->willReturnSelf();
+        $process->expects($this->at(1))
+                ->method('mustRun')
+                ->willReturnSelf();
+
+        $instance = $this->newInstance(['newCommand']);
+        $instance->expects($this->atLeastOnce())
+                 ->method('newCommand')
+                 ->with('wp --allow-root db import -', ['-i'])
+                 ->willReturn($process);
+
+        $instance->importDatabase($fh);
+    }
+
+    public function testExportDatabase()
+    {
+        $vfs = vfsStream::setup('root', null);
+        $exportPath = "{$vfs->url()}/database.sql";
+        $fh = fopen($exportPath, 'w');
+
+        $exportData = 'Output from "wp db export" command which should be written to the SQL file';
+
+        $process = $this->createMock(Process::class);
+        $process->expects($this->atLeastOnce())
+                ->method('disableOutput')
+                ->willReturnSelf();
+        $process->expects($this->atLeastOnce())
+                ->method('mustRun')
+                ->with($this->callback('is_callable'))
+                ->willReturnCallback(function($callback) use ($exportData) {
+                    // Split export data into chunks to emulate command output being streamed to the callback via multiple invocations
+                    $exportChunks = str_split($exportData, 20);
+                    foreach ($exportChunks as $chunk) {
+                        $callback(Process::OUT, $chunk);
+                    }
+                });
+
+        $instance = $this->newInstance(['newCommand']);
+        $instance->expects($this->atLeastOnce())
+                 ->method('newCommand')
+                 ->with('wp --allow-root db export -')
+                 ->willReturn($process);
+
+        $instance->exportDatabase($fh);
+        fclose($fh);
+
+        $fileContents = file_get_contents($exportPath);
+        $this->assertEquals($exportData, $fileContents);
+    }
+
+    /**
+     * Create a mock AbstrastInstance object
+     *
+     * @param array $mockMethods Methods to mock
+     *
+     * @return \PHPUnit\Framework\MockObject\MockObject
+     * @throws ReflectionException
+     */
+    private function newInstance($mockMethods = [])
+    {
+        return $this->getMockForAbstractClass(
+            AbstractInstance::class,
+            [],
+            '',
+            false,
+            true,
+            true,
+            $mockMethods
+        );
     }
 }
